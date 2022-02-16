9484acea68ba18f145c8d4607cb92122b0ed4701
Collin Nehemiah <collin.nehemia@ucglobalprograms.org>
2022-02-16 22:55:24 +0200

    NMA-61 -openhim configuration

--- ptracker-odk-central-mediator/src/index.js
+++ ptracker-odk-central-mediator/src/index.js
@@ -8,43 +8,43 @@ const cron = require("node-cron");
 const app = express();
 
 // OpenHIM
-// const { registerMediator, activateHeartbeat, fetchConfig } = require('openhim-mediator-utils');
-// let queryParams;
-// const openhimVars = privateConfig.openhimConfig;
-// const openhimConfig = {
-//     username: openhimVars.username,
-//     password: openhimVars.password,
-//     apiURL: openhimVars.apiURL,
-//     trustSelfSigned: true,
-//     urn: mediatorConfig.urn
-// }
-// const emitter = activateHeartbeat(openhimConfig);
+const { registerMediator, activateHeartbeat, fetchConfig } = require('openhim-mediator-utils');
+let queryParams;
+const openhimVars = privateConfig.openhimConfig;
+const openhimConfig = {
+    username: openhimVars.username,
+    password: openhimVars.password,
+    apiURL: openhimVars.apiURL,
+    trustSelfSigned: true,
+    urn: mediatorConfig.urn
+}
+const emitter = activateHeartbeat(openhimConfig);
 
-// registerMediator(openhimConfig, mediatorConfig, err => {
-//     if (err) {
-//         throw new Error(`Failed to register mediator. Check your Config. ${err}`)
-//     }
-// });
+registerMediator(openhimConfig, mediatorConfig, err => {
+    if (err) {
+        throw new Error(`Failed to register mediator. Check your Config. ${err}`)
+    }
+});
 
-// fetchConfig(openhimConfig, (err, initialConfig) => {
-//     if (err) {
-//         throw new Error(`Failed to fetch initial config. ${err}`)
-//     }
-//     console.log('Initial Config: ', JSON.stringify(initialConfig))
-//     queryParams = initialConfig.labResultsParams
-// });
+fetchConfig(openhimConfig, (err, initialConfig) => {
+    if (err) {
+        throw new Error(`Failed to fetch initial config. ${err}`)
+    }
+    console.log('Initial Config: ', JSON.stringify(initialConfig))
+    queryParams = initialConfig.labResultsParams
+});
 
-// emitter.on('error', err => {
-//     console.error('Heartbeat failed: ', err)
-// });
+emitter.on('error', err => {
+    console.error('Heartbeat failed: ', err)
+});
 
-// emitter.on('config', newConfig => {
-//     console.log('Received updated config:', JSON.stringify(newConfig))
-//     queryParams = newConfig.labResultsParams
-// });
+emitter.on('config', newConfig => {
+    console.log('Received updated config:', JSON.stringify(newConfig))
+    queryParams = newConfig.labResultsParams
+});
 
-// function getQueryParameters() { return queryParams }
-// module.exports = { getQueryParameters };
+function getQueryParameters() { return queryParams }
+module.exports = { getQueryParameters };
 
 
 app.all('*', async(req, res) => {
