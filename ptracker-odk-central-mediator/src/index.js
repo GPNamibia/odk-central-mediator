ff5e191f2ffe230197c75e0148293222b7c9e877
Collin Nehemiah <collin.nehemia@ucglobalprograms.org>
2022-02-23 14:02:18 +0200

    NMA-61(OpenHIM configurations)

--- ptracker-odk-central-mediator/src/index.js
+++ ptracker-odk-central-mediator/src/index.js
@@ -1,49 +1,12 @@
 const express = require("express");
 const ptrackerData = require('./odkCentral/odk-central.js');
 const privateConfig = require('./config/private-config.json');
-const mediatorConfig = require('./config/mediator-config.json')
+const { getQueryParameters } = require('./openhim/initialize');
 const db = require('./models');
 const app = express();
 
-// OpenHIM
-const { registerMediator, activateHeartbeat, fetchConfig } = require('openhim-mediator-utils');
-let queryParams;
-const openhimVars = privateConfig.openhimConfig;
-const openhimConfig = {
-    username: openhimVars.username,
-    password: openhimVars.password,
-    apiURL: openhimVars.apiURL,
-    trustSelfSigned: true,
-    urn: mediatorConfig.urn
-}
-const emitter = activateHeartbeat(openhimConfig);
-
-registerMediator(openhimConfig, mediatorConfig, err => {
-    if (err) {
-        throw new Error(`Failed to register mediator. Check your Config. ${err}`)
-    }
-});
-
-fetchConfig(openhimConfig, (err, initialConfig) => {
-    if (err) {
-        throw new Error(`Failed to fetch initial config. ${err}`)
-    }
-    console.log('Initial Config: ', JSON.stringify(initialConfig))
-    queryParams = initialConfig.labResultsParams
-});
-
-emitter.on('error', err => {
-    console.error('Heartbeat failed: ', err)
-});
-
-emitter.on('config', newConfig => {
-    console.log('Received updated config:', JSON.stringify(newConfig))
-    queryParams = newConfig.labResultsParams
-});
-
-function getQueryParameters() { return queryParams }
-module.exports = { getQueryParameters };
-
+//openHIM
+getQueryParameters();
 
 app.all('*', async(req, res) => {
     // Starts when a new request is triggered by the polling channel
